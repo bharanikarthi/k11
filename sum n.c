a4fe74c7169cee26b64a0065ae8edab179a43d1d
bharanikarthi <47816055+bharanikarthi@users.noreply.github.com>
2019-02-28 11:32:37 +0530

    Create sum n.c

---
+++ sum n.c
@@ -0,0 +1,14 @@
+#include<stdlib.h>
+#include<conio.h>
+void main()
+{
+int p,i,sum=0;
+printf("enter the positive integer");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+sum +=1;
+}
+printf("sum=%d",sum);
+getch();
+}
